5a4925b64a9a918e59f3829d6f0fd9fa1a0261b9
dt0wns <dennis@dennistowns.com>
2024-02-09 12:17:23 -0500

    Spider refactor

--- flispi_scrapy/spiders/landbank_spider.py
+++ flispi_scrapy/spiders/landbank_spider.py
@@ -3,10 +3,9 @@ import scrapy
 from ..models.scrapy.property import Property
 from ..models.sql.property import PropertyEntity
 
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import psycopg2
 import os
 from dotenv import load_dotenv
 from datetime import datetime
@@ -30,30 +29,27 @@ class LandBankSpider(scrapy.Spider):
 
     def parse(self, response):
         for row in response.xpath('//tr'):
-            property_item = Property()
-            property_item['parcel_id'] = str(row.xpath('./td[1]/label/a/text()').get()).replace('-','')
-            property_item['address'] = row.xpath('./td[2]/div/text()').get()
-            property_item['city'] = row.xpath('./td[3]/div/text()').get()
-            property_item['zip'] = row.xpath('./td[4]/div/text()').get()
-            
-            property_item['property_class'] = row.xpath('./td[5]/div/text()').get()
-            if(property_item['parcel_id']):
-                yield property_item
-        
+            parcel_id = row.xpath('./td[1]/label/a/text()').get(default="").replace('-', '')
+            address = row.xpath('./td[2]/div/text()').get()
+            city = row.xpath('./td[3]/div/text()').get()
+            zip_code = row.xpath('./td[4]/div/text()').get()
+            property_class = row.xpath('./td[5]/div/text()').get()
 
-        # Handle pagination
-        next_link_exists = response.xpath('//a[contains(@href, "javascript:document.LRCquery.LRCpage.value=\'next\';document.LRCquery.submit();")]').get()
-        print('Pagination Found, executing...', next_link_exists)
-        if next_link_exists:
-            formdata = {
-                'LRCpage': 'next',
-                'LRCsearch': 'redo',
-            }
+            if parcel_id:
+                yield Property(
+                    parcel_id=parcel_id,
+                    address=address,
+                    city=city,
+                    zip=zip_code,
+                    property_class=property_class,
+                )
 
+        next_page_js = response.xpath('//a[contains(@href, "javascript:document.LRCquery.LRCpage.value=\'next\';document.LRCquery.submit();")]')
+        if next_page_js:
             yield scrapy.FormRequest.from_response(
                 response,
-                formname='LRCquery',  # replace with actual form name
-                formdata=formdata,
+                formname='LRCquery',
+                formdata={'LRCpage': 'next', 'LRCsearch': 'redo'},
                 callback=self.parse
             )
 
@@ -70,42 +66,29 @@ class PriceSpider(scrapy.Spider):
 
         # #Get specific environment variables
         prod_db_url = os.environ['PROD_POSTGRESS_URL']
-        self.connection_string = prod_db_url
-        self.engine = create_engine(self.connection_string)
+        self.engine = create_engine(prod_db_url)
         Session = sessionmaker(bind=self.engine)
-        session = Session()
-        parcel_ids = [row.parcel_id for row in session.query(PropertyEntity.parcel_id).all() if row.parcel_id != '0' and row.parcel_id != 'None']
-        session.close()
-        # Test Url
-        # yield scrapy.Request(url='https://www.thelandbank.org/property_sheet.asp?pid=4728355061&loc=1&from=main', 
-        #     callback=self.parse,
-        #     meta={'parcel_id': '4728355061'})
+        with Session() as session:
+            parcel_ids = [row.parcel_id for row in session.query(PropertyEntity.parcel_id).all() if row.parcel_id not in ('0', 'None')]
         for pid in parcel_ids:
-        #     # Assuming parcelId is a unique identifier, construct the URL
-        #     # https://www.thelandbank.org/property_sheet.asp?pid=0404300022&loc=2&from=main
-            yield scrapy.Request(url='https://www.thelandbank.org/property_sheet.asp?pid=' + str(pid) + '&loc=2&from=main', 
-                        callback=self.parse, 
-                        meta={'parcel_id': pid})
+            url = 'https://www.thelandbank.org/property_sheet.asp?pid={pid}&loc=2&from=main'
+            yield scrapy.Request(url=url, callback=self.parse, meta={'parcel_id': pid})
+        
+        # Test Url
+        # yield scrapy.Request(url='https://www.thelandbank.org/property_sheet.asp?pid=4626153017&loc=2&from=main', 
+        #     callback=self.parse,
+        #     meta={'parcel_id': '4626153017'})
 
     # Grab the data on the search details page (property_sheet.asp)
     def parse(self, response):
         starting_price = response.xpath('//table[@class="infotab"]/tr[1]/td[2]/text()').get()
+        price = self.extract_price(starting_price)
 
-        link_selector = response.xpath("//a[contains(text(), 'featured')]/@href")
-        property_item = Property()
-        property_item['parcel_id'] = response.meta['parcel_id']
-
-        if starting_price:
-            if '$' in starting_price:
-                starting_price = int(starting_price.replace('$', '').replace(',', '').strip())
-                property_item['price'] = starting_price
-            else:
-                property_item['price'] = 0
-
+        link = response.xpath("//a[contains(text(), 'featured')]/@href").get()
+        property_item = Property(parcel_id=response.meta['parcel_id'], price=price)
 
         # Extract the href attribute from the selected <a> tag
-        link = link_selector.get()
-        if(link):
+        if link:
             # Follow the link to the featured property page and grab more info
             yield scrapy.Request(url='https://www.thelandbank.org/' + link, callback=self.extract_featured_data, meta={'property_item': property_item})
         else:
@@ -115,102 +98,86 @@ class PriceSpider(scrapy.Spider):
     def extract_featured_data(self, response):
         # Follow the link to the featured property page and grab more info
         # Select the div by class name, then the ul within it
-        print('extract_featured_data', response)
-        property_details = response.meta['property_item']
-        property_details['featured'] = True
-        property_info_list = response.xpath("//div[@class='2u 12u']/ul")
-        suggested_offer_price = response.xpath("//h2[contains(text(), 'Starting Offer')]/text()").get()
-        next_showtime = response.xpath("//p[contains(text(), '2023')]/text()").get()
-        exterior_repairs_text = response.xpath("//article[@id='content']/ul[position()=1]")
-        interior_repairs_text = response.xpath("//article[@id='content']/ul[position()=2]")
-
-        exterior_repairs = []
-        interior_repairs = []
         
-        if exterior_repairs_text.xpath('./li'):
-            for li in exterior_repairs_text.xpath('./li'):
-                text = li.xpath('normalize-space(.)').get()
-                exterior_repairs.append(text)
-            property_details['exterior_repairs'] = interior_repairs
+        # yield property_details
+        property_details = response.meta['property_item']
+        property_details.update({
+            'featured': True,
+            'exterior_repairs': self.extract_list_items(response.xpath("//article[@id='content']/ul[position()=1]/li")),
+            'interior_repairs': self.extract_list_items(response.xpath("//article[@id='content']/ul[position()=2]/li")),
+            'images': self.extract_images(response),
+            'next_showtime': self.extract_next_showtime(response),
+            'price': self.extract_featured_price(response) if not property_details['price'] else property_details['price'],
+            **self.extract_property_features(response),
+        })
+        print(property_details)
+        yield property_details
 
-        if interior_repairs_text.xpath('./li'):
-            for li in interior_repairs_text.xpath('./li'):
-                text = li.xpath('normalize-space(.)').get()
-                interior_repairs.append(text)
-            property_details['interior_repairs'] = exterior_repairs
+    
+    # FEATURED DATA HELPER FUNCTIONS
+    
+    def extract_price(self, price_str):
+        if price_str and '$' in price_str:
+            return int(price_str.replace('$', '').replace(',', '').strip())
+        return 0
 
+    def extract_featured_price(self, response):
+        suggested_offer_price = response.xpath("//h2[contains(text(), 'Starting Offer')]/text()").get()
+        if suggested_offer_price and 'negotiable' not in str(suggested_offer_price).lower():
+            suggested_offer_price = int(suggested_offer_price.split(':')[1].replace(',', '').replace("$", "").strip())
+            return int(suggested_offer_price)
+        return 0
+    
+    def extract_list_items(self, selector):
+        return [li.xpath('normalize-space(.)').get() for li in selector]
+
+    def extract_images(self, response):
+        images = response.xpath("//a[@class='sslightbox']/@href | //div[@class='bss-slides ccss1']/figure/img/@src").getall()
+        return ['https://www.thelandbank.org/' + img if not img.startswith('http') else img for img in images]
+    
+    def extract_next_showtime(self, response):
+        next_showtime = response.xpath("//p[contains(text(), '2024')]/text()").get()
         if next_showtime:
             match = re.search(r'(\w+,\s+\w+\s+\d{1,2},\s+\d{4});\s+(\d{1,2}:\d{2}\s+[ap]\.m\.)', next_showtime)
             if match:
                 date_to_parse = match.group(1) + ' ' + match.group(2)
                 date_string = date_to_parse.replace('p.m.', 'PM').replace('a.m.', 'AM')
                 date_object = datetime.strptime(date_string, '%A, %B %d, %Y %I:%M %p')
-                property_details['next_showtime'] = date_object
+                return date_object
             else:
                 print("SHOWTIME: Date and time format not recognized")
+        return None
+    
+    def extract_property_features(self, response):
+        features = {}
 
-
-        # Initialize a dictionary to hold the extracted property details
-
-        if suggested_offer_price and 'negotiable' not in str(suggested_offer_price).lower():
-            suggested_offer_price = int(suggested_offer_price.split(':')[1].replace(',', '').replace("$", "").strip())
-            property_details['price'] = int(suggested_offer_price)
-
-        carousel_div = response.xpath("//div[@class='bss-slides ccss1']")
-        lightbox_a_tag = response.xpath("//a[@class='sslightbox']/@href")
-
-
-        for tag in lightbox_a_tag:
-            image = tag.get()
-            if image:
-                if 'http' not in image:
-                    image = 'https://www.thelandbank.org/' + image
-                if 'images' not in property_details:
-                    property_details['images'] = []
-                property_details['images'].append(image)
-
-        for figure in carousel_div.xpath('./figure'):
-            image = figure.xpath('./img/@src').get()
-            if image:
-                if 'http' not in image:
-                    image = 'https://www.thelandbank.org/' + image
-                if 'images' not in property_details:
-                    property_details['images'] = []
-                property_details['images'].append(image)
-
-
-        property_features = {}
-
-        # Iterate over all the li tags within the ul
+        property_info_list = response.xpath("//div[@class='2u 12u']/ul")
         for li in property_info_list.xpath('./li'):
-            # Extract the text content of each li tag
             text = li.xpath('normalize-space(.)').get()
-
-            # Check for specific keywords and extract the data
             if 'Square feet:' in text:
-                property_details['square_feet'] = int(text.split('Square feet:')[1].strip())
-            elif 'Bedrooms' in text:
-                property_details['bedrooms'] = int(text.split('Bedrooms')[0].strip())
-            elif 'Bathrooms' in text:
-                property_details['bathrooms'] = int(text.split('Bathrooms')[0].strip())
+                features['square_feet'] = int(text.split('Square feet:')[1].strip())
+            elif 'Bedrooms' in text or "Bedroom" in text:
+                text.replace('s', '')
+                features['bedrooms'] = int(text.split('Bedroom')[0].strip())
+            elif 'Bathroom' in text or "Bathrooms" in text:
+                text.replace('s', '')
+                features['bathrooms'] = int(text.split('Bathroom')[0].strip())
             elif 'Year built:' in text:
-                property_details['year_built'] = text.split('Year built:')[1].strip()
+                features['year_built'] = text.split('Year built:')[1].strip()
             elif 'Acres' in text:
-                property_details['lot_size'] = float(text.split('Acres')[0].strip())
+                features['lot_size'] = float(text.split('Acres')[0].strip())
             elif 'Stories:' in text:
-                property_details['stories'] = int(text.split('Stories')[0].strip())
+                features['stories'] = int(text.split('Stories')[0].strip())
             elif 'Garage' in text:
-                property_details['garage'] = text.split('Garage')[0].strip()
-            else:
-                property_features[text] = True
-        
-        yield property_details
+                features['garage'] = text.split('Garage')[0].strip()
+        return features 
+
 settings = get_project_settings()
 runner = CrawlerRunner(settings)
 configure_logging(settings)
 @defer.inlineCallbacks
 def crawl():
-    yield runner.crawl(LandBankSpider)
+    # yield runner.crawl(LandBankSpider)
     yield runner.crawl(PriceSpider)
     reactor.stop()
 
